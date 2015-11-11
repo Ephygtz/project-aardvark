3ea06c83a2dd9d57fb19ec3ef1a82968a212259b
Ephraim <ephygtz@gmail.com>
2015-11-11 16:44:48 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var dispatch = require('dispatch');
+
+
+var server = http.createServer(
+			dispatch({
+				'/' : function(request, response) {
+						message = {
+							type: 'customer',
+							text: 'Hi, how are you'
+						};
+						response.writeHead(200, {
+							'Content-type': 'application/json',
+							'Access-Control-Allow-Origin': 'http://127.0.0.1:9000'
+						});
+						response.end(JSON.stringify(message));
+						console.log('Visiting %s', request.url);
+						response.end('This is the root');
+				},
+				
+			})
+		);
+
+server.listen(8081, function () {
+	console.log('server running on 127.0.0.1:8081');
+});
