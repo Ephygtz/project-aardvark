4203c3715553f4ae0d3c6c194cf86e7b28b0c4e2
Ephraim <ephygtz@gmail.com>
2015-12-02 11:38:14 +0300

    Add retrieve method from MongoDB

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ app.get('/movies', function(req, res) {
 	Movie.find(function(err, movies) {
 		if (err) {
 			console.log(err);
-		}else {	
-    res.json(movies);	
+		}else {
+    res.json(movies);
 		}
 	});
 });
@@ -44,7 +44,7 @@ app.get('/movies', function(req, res) {
 app.post('/movies/new', function(req, res) {
 	console.log(req.body);
 	formdata = req.body;
-	 
+
 	 var movie = new Movie(formdata);
 	 movie.save(function(err, movie) {
 	 	if(err) {
@@ -56,151 +56,17 @@ app.post('/movies/new', function(req, res) {
 	 });
 });
 
+app.get('/movies/:id', function(request, response) {
+  var movieId = request.params.id;
 
-// var server = http.createServer(
-//     dispatch({
+	//retrieve the movie from mongodb
+	Movie.findById(movieId, function (err, movie) {
+		if (err) return console.log(err);
 
-        // '/movies': {
+		response.json(movie);
+	});
+});
 
-        //     'GET /': function(request, response, next) {
-        //         movies = [{
-        //                 title: 'Black Mass',
-        //                 category: ['thriller', 'action', 'true-story'],
-        //                 main_actors: [{
-        //                     first_name: 'Johnny',
-        //                     last_name: 'Depp'
-        //                 }, {
-        //                     first_name: 'Benedict',
-        //                     last_name: 'Cumberbatch'
-
-
-        //                 }]
-
-        //             },
-
-        //             {
-        //                 title: 'Avengers: age of Ultron',
-        //                 category: ['fanatsy', 'action'],
-        //                 main_actors: [{
-        //                     first_name: 'Chris',
-        //                     last_name: 'Evans'
-
-
-        //                 }, {
-        //                     first_name: 'Robert',
-        //                     last_name: 'Jr'
-
-
-
-        //                 }]
-
-
-        //             }, {
-        //                 title: 'Straight-Outta-Compton',
-        //                 category: ['drama', ''],
-        //                 main_actors: [{
-        //                     first_name: 'Oshea',
-        //                     last_name: 'Jackson'
-
-
-
-        //                 }, {
-
-        //                     first_name: 'aldis',
-        //                     last_name: 'Hodge'
-
-
-
-        //                 }]
-
-
-        //             }
-
-        //         ];
-
-
-        //         response.end(JSON.stringify(movies));
-        //     },
-        //     '/POST': function(request, response, next) {
-        //         //get parameters from the form 
-        //         var formdata = '';
-        //         request.on('data', function(chunk) {
-        //             formdata = querystring.parse(chunk.toString())
-
-        //         });
-
-//                 request.on('end', function() {
-//                     console.log(formdata);
-//                     //create an instance of a movie
-//                     var movie = new Movie({
-//                         title: formdata.title,
-//                         year_of_release: formdata.year_of_release
-//                     });
-
-
-
-
-
-//                 })
-
-
-//                 //inside this code we want to
-//                 //1.create an instance of a movie 
-//                 //syntax for creation of a new instance ...... var name = new name({})
-//                 var movie = new Movie({
-//                     title: formdata.title,
-//                     year_of_release: formdata.year_of_release
-
-
-
-//                 });
-
-//             }
-
-
-//         }
-//     })
-// );
-// //Create a schema
-// var pizzaSchema = mongoose.Schema({
-// 	name: String,
-// 	price: Number,
-// 	created_at: {type: Date, default: Date.now()}
-// });
-
-
-// //Compile our model
-// var Pizza = mongoose.model('Pizza', pizzaSchema);
-
- //using the model
-// var pizza = new Pizza({name: 'Vegetarian', price: 1000});
-
-//create the document
-// pizza.save(function(err, pizza) {
-// 	if(err){
-// 		return console.error('Your Pizza wasnt saved: ', pizza);
-// 	}
-// 	console.log('Your pizza was succesfully saved');
-// });
-
-// var server = http.createServer(
-// 			dispatch({
-// 				'/' : function(request, response) {
-// 						message = {
-// 							type: 'customer',
-// 							text: 'Hi, how are you'
-// 						};
-// 						response.writeHead(200, {
-// 							'Content-type': 'application/json',
-// 							'Access-Control-Allow-Origin': 'http://127.0.0.1:9000'
-// 						});
-// 						response.end(JSON.stringify(message));
-// 						console.log('Visiting %s', request.url);
-// 						response.end('This is the root');
-// 				},
-
-// 			})
-// 		);
 
 app.listen(8081, function() {
     console.log('server running on http://127.0.0.1:8081');
