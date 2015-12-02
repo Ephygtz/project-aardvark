1d8ef24583fd82b9610d69ed0184e097ed3859b5
Ephraim <ephygtz@gmail.com>
2015-12-02 15:45:19 +0300

    Add rating to the schema and Put, Post, Delete methods

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ var express = require('express');
 var app = express();
 
 app.use(function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, content-type, Accept');
-	next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, content-type, Accept');
+  next();
 });
 
 //Express Middleware
@@ -20,54 +20,88 @@ var bodyParser = require('body-parser');
 
 //define our schema
 var movieSchema = mongoose.Schema({
-        title: String,
-        year_of_release: Number,
-        category: String
+  title: String,
+  year_of_release: Number,
+  category: String,
+  rating: {
+    type: Number,
+    default: 0,
+    min: 0,
+    max: 12
+  }
+});
 
-    });
-    //compile our model
+//compile our model
 var Movie = mongoose.model('Movie', movieSchema);
 
-
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get('/movies', function(req, res) {
-	Movie.find(function(err, movies) {
-		if (err) {
-			console.log(err);
-		}else {
-    res.json(movies);
-		}
-	});
+  Movie.find(function(err, movies) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(movies);
+    }
+  });
 });
 
 app.post('/movies/new', function(req, res) {
-	console.log(req.body);
-	formdata = req.body;
+  console.log(req.body);
+  formdata = req.body;
 
-	 var movie = new Movie(formdata);
-	 movie.save(function(err, movie) {
-	 	if(err) {
-	 		console.log(err);
-	 	} else {
-	 		console.log('succesfully saved the movie');
-	 		res.redirect('/movies');
-	 	}
-	 });
+  var movie = new Movie(formdata);
+  movie.save(function(err, movie) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('succesfully saved the movie');
+      res.redirect('/movies');
+    }
+  });
 });
 
 app.get('/movies/:id', function(request, response) {
-  var movieId = request.params.id;
+  movieId = request.params.id;
 
-	//retrieve the movie from mongodb
-	Movie.findById(movieId, function (err, movie) {
-		if (err) return console.log(err);
+  //retrieve the movie from mongodb
+  Movie.findById(movieId, function(err, movie) {
+    if (err) return console.log(err);
 
-		response.json(movie);
-	});
+    response.json(movie);
+  });
 });
 
+app.put('/movies/:id', function(request, response) {
+  movieId = request.params.id;
+  userRating = request.body.rating;
+
+  //Update the movie from Mongodb
+  Movie.findById(movieId, function(err, movie) {
+    if (err) return console.log(err);
+
+    movie.rating = userRating;
+    movie.save(function(err, movie) {
+      if (err) return console.log(err);
+
+      response.json(movie);
+    });
+  });
+});
+
+app.delete('/movies/:id', function(request, response) {
+  movieId = request.params.id;
+
+  //Delete the movie from Mongodb
+  Movie.remove({_id: movieId}, function(err) {
+    if (err) return console.log(err);
+
+      response.send('movie was deleted');
+  });
+});
 
 app.listen(8081, function() {
-    console.log('server running on http://127.0.0.1:8081');
+  console.log('server running on http://127.0.0.1:8081');
 });
